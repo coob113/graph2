9096bd6a5c32ccb610bf01e6526f3cf165499a8a
coob113 <coob1113@gmail.com>
2017-11-27 11:48:17 +0400

    a

---
+++ README.md
@@ -0,0 +1,85 @@
+## aframe-forcegraph-component
+
+[![Version](http://img.shields.io/npm/v/aframe-forcegraph-component.svg?style=flat-square)](https://npmjs.org/package/aframe-forcegraph-component)
+[![License](http://img.shields.io/npm/l/aframe-forcegraph-component.svg?style=flat-square)](https://npmjs.org/package/aframe-forcegraph-component)
+
+A 3D Force-Directed Graph component for [A-Frame](https://aframe.io).
+
+<p align="center">
+     <img width="80%" src="https://vasturiano.github.io/aframe-forcegraph-component/examples/large-graph/preview.png"></a>
+</p>
+
+An A-Frame entity component to represent a graph data structure in a VR environment using a force-directed iterative layout.
+Uses [d3-force-3d](https://github.com/vasturiano/d3-force-3d) for the layout physics engine.
+
+See also the [standalone VR component version](https://github.com/vasturiano/3d-force-graph-vr).
+
+### API
+
+| Property           | Description                                                                                                                | Default Value |
+| ------------------ | -------------------------------------------------------------------------------------------------------------------------- | ------------- |
+| json-url           | URL of JSON file to load graph data directly from. Will override content of the *nodes* and *links* component properties so either use one or the other. JSON should contain an object with two list properties: *nodes* and *links*.  |               |
+| nodes              | List of node objects. *Example*: ```[{"id": 1, "name": "first"}, {"id": 2, "name": "second"}]```                           | []            |
+| links              | List of link objects. *Example*: ```[{"source": 1, "target": 2}]```                                                        | []            |
+| num-dimensions     | Number of dimensions to run the force simulation on (1, 2 or 3).                                                           | 3             |
+| node-rel-size      | Node sphere volume per val unit.                                                                                           | 4             |
+| node-resolution    | Geometric resolution of each node, expressed in how many slice segments to divide the circumference. Higher values yield smoother spheres. | 8 |
+| line-opacity       | Opacity of links.                                                                                                          | 0.2           |
+| auto-color-by      | Node object accessor function (`fn(node)`) or attribute (e.g. `'type'`) to automatically group colors by. Only affects nodes without a color attribute. |               |
+| id-field           | Node object attribute name referring to unique node id (used in link objects source/target).                               | id            |
+| val-field          | Node object accessor function, attribute or a numeric constant for the node numeric value (affects sphere volume).         | val           |
+| name-field         | Node object accessor function or attribute for name (shown in label).                                                      | name          |
+| color-field        | Node object accessor function or attribute for node color (affects sphere color).                                          | color         |
+| link-source-field  | Link object attribute name referring to id of source node.                                                                 | source        |
+| link-target-field  | Link object attribute name referring to id of target node.                                                                 | target        |
+| link-color-field   | Link object accessor function or attribute for line color.                                                                 | color         |
+| force-engine       | Which force-simulation engine to use ([*d3*](https://github.com/vasturiano/d3-force-3d) or [*ngraph*](https://github.com/anvaka/ngraph.forcelayout)).  | d3             |
+| warmup-ticks       | How many times to tick the force simulation engine at ignition before starting to render.                                  | 0             |
+| cooldown-ticks     | How many times to tick the force simulation engine after rendering begins before stopping and freezing the engine.         | Infinity      |
+| cooldown-time      | How much time (ms) to tick the force simulation engine after rendering begins before stopping and freezing the engine.     | 15000         |
+
+### Installation
+
+#### Browser
+
+Install and use by directly including the [browser files](dist):
+
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://aframe.io/releases/0.5.0/aframe.min.js"></script>
+  <script src="https://unpkg.com/aframe-forcegraph-component/dist/aframe-forcegraph-component.min.js"></script>
+</head>
+
+<body>
+  <a-scene>
+    <a-entity forcegraph="json-url: myGraphData.json"></a-entity>
+  </a-scene>
+</body>
+```
+
+<!-- If component is accepted to the Registry, uncomment this. -->
+<!--
+Or with [angle](https://npmjs.com/package/angle/), you can install the proper
+version of the component straight into your HTML file, respective to your
+version of A-Frame:
+
+```sh
+angle install aframe-forcegraph-component
+```
+-->
+
+#### npm
+
+Install via npm:
+
+```bash
+npm install aframe-forcegraph-component
+```
+
+Then require and use.
+
+```js
+require('aframe');
+require('aframe-forcegraph-component');
+```
---
+++ index.js
@@ -0,0 +1,301 @@
+/* global AFRAME */
+
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+var qwest = require('qwest'),
+    accessorFn = require('accessor-fn'),
+    tinyColor = require('tinycolor2'),
+    d3Chromatic = require('d3-scale-chromatic'),
+    d3 = require('d3-force-3d'),
+    ngraph = {
+      graph: require('ngraph.graph'),
+      forcelayout: require('ngraph.forcelayout'),
+      forcelayout3d: require('ngraph.forcelayout3d')
+    };
+
+var parseAccessor = function(prop) {
+  var geval = eval; // Avoid using eval directly https://github.com/rollup/rollup/wiki/Troubleshooting#avoiding-eval
+  try {
+    var evalled = geval('(' + prop + ')');
+    if (evalled instanceof Function) {
+      prop = evalled;
+    }
+  }
+  catch (e) {} // Can't eval, not a function
+  return prop;
+};
+
+/**
+ * 3D Force-Directed Graph component for A-Frame.
+ */
+AFRAME.registerComponent('forcegraph', {
+  schema: {
+    jsonUrl: {type: 'string', default: ''},
+    nodes: {parse: JSON.parse, default: '[]'},
+    links: {parse: JSON.parse, default: '[]'},
+    numDimensions: {type: 'number', default: 3},
+    nodeRelSize: {type: 'number', default: 4}, // volume per val unit
+    nodeResolution: {type: 'number', default: 8}, // how many slice segments in the sphere's circumference
+    lineOpacity: {type: 'number', default: 0.2},
+    autoColorBy: {parse: parseAccessor, default: ''}, // color nodes with the same field equally
+    idField: {type: 'string', default: 'id'},
+    valField: {parse: parseAccessor, default: 'val'},
+    nameField: {parse: parseAccessor, default: 'name'},
+    colorField: {parse: parseAccessor, default: 'color'},
+    linkSourceField: {type: 'string', default: 'source'},
+    linkTargetField: {type: 'string', default: 'target'},
+    linkColorField: {parse: parseAccessor, default: 'color'},
+    forceEngine: {type: 'string', default: 'd3'}, // 'd3' or 'ngraph'
+    warmupTicks: {type: 'int', default: 0}, // how many times to tick the force engine at init before starting to render
+    cooldownTicks: {type: 'int', default: 1e18}, // Simulate infinity (int parser doesn't accept Infinity object)
+    cooldownTime: {type: 'int', default: 15000} // ms
+  },
+
+  init: function () {
+    var state = this.state = {}; // Internal state
+
+    // Add info msg
+    state.infoEl = document.createElement('a-text');
+    state.infoEl.setAttribute('position', '0 -0.1 -1'); // Canvas center
+    state.infoEl.setAttribute('width', 1);
+    state.infoEl.setAttribute('align', 'center');
+    state.infoEl.setAttribute('color', 'lavender');
+
+    // Setup tooltip
+    state.tooltipEl = document.createElement('a-text');
+    state.tooltipEl.setAttribute('position', '0 -0.5 -1'); // Aligned to canvas bottom
+    state.tooltipEl.setAttribute('width', 2);
+    state.tooltipEl.setAttribute('align', 'center');
+    state.tooltipEl.setAttribute('color', 'lavender');
+    state.tooltipEl.setAttribute('value', '');
+
+    // Get camera dom element and attach fixed view elements to camera
+    var cameraEl = document.querySelector('a-entity[camera], a-camera');
+    cameraEl.appendChild(state.infoEl);
+    cameraEl.appendChild(state.tooltipEl);
+
+    // Keep reference to Three camera object
+    state.cameraObj = cameraEl.object3D.children
+        .filter(function(child) { return child.type === 'PerspectiveCamera' })[0];
+
+    // On camera switch
+    this.el.sceneEl.addEventListener('camera-set-active', function(evt) {
+      // Switch camera reference
+      state.cameraObj = evt.detail.cameraEl.components.camera.camera;
+    });
+
+    // Add D3 force-directed layout
+    state.d3ForceLayout = d3.forceSimulation()
+        .force('link', d3.forceLink())
+        .force('charge', d3.forceManyBody())
+        .force('center', d3.forceCenter())
+        .stop();
+  },
+
+  remove: function () {
+    // Clean-up elems
+    this.state.infoEl.remove();
+    this.state.tooltipEl.remove();
+  },
+
+  update: function (oldData) {
+    var comp = this,
+        elData = this.data,
+        diff = AFRAME.utils.diff(elData, oldData);
+
+    comp.state.onFrame = null; // Pause simulation
+    comp.state.infoEl.setAttribute('value', 'Loading...'); // Add loading msg
+
+    if ('jsonUrl' in diff && elData.jsonUrl) {
+      // (Re-)load data
+      qwest.get(elData.jsonUrl).then(function(_, json) {
+        elData.nodes = json.nodes;
+        elData.links = json.links;
+
+        comp.update(elData);  // Force re-update
+      });
+    }
+
+    if (elData.autoColorBy) {
+      // Auto add color to uncolored nodes
+      autoColorNodes(elData.nodes, accessorFn(elData.autoColorBy), elData.colorField);
+    }
+
+    // parse links
+    elData.links.forEach(function(link) {
+      link.source = link[elData.linkSourceField];
+      link.target = link[elData.linkTargetField];
+    });
+
+    // Add children entities
+    var el3d = this.el.object3D;
+    while(el3d.children.length){ el3d.remove(el3d.children[0]) } // Clear the place
+
+    var nameAccessor = accessorFn(elData.nameField);
+    var valAccessor = accessorFn(elData.valField);
+    var colorAccessor = accessorFn(elData.colorField);
+    var sphereGeometries = {}; // indexed by node value
+    var sphereMaterials = {}; // indexed by color
+    elData.nodes.forEach(function(node) {
+      var val = valAccessor(node) || 1;
+      if (!sphereGeometries.hasOwnProperty(val)) {
+        sphereGeometries[val] = new THREE.SphereGeometry(Math.cbrt(val) * elData.nodeRelSize, elData.nodeResolution, elData.nodeResolution);
+      }
+
+      var color = colorAccessor(node);
+      if (!sphereMaterials.hasOwnProperty(color)) {
+        sphereMaterials[color] = new THREE.MeshLambertMaterial({
+          color: colorStr2Hex(color || '#ffffaa'),
+          transparent: true,
+          opacity: 0.75
+        });
+      }
+
+      var sphere = new THREE.Mesh(sphereGeometries[val], sphereMaterials[color]);
+
+      sphere.name = nameAccessor(node); // Add label
+
+      el3d.add(node.__sphere = sphere);
+    });
+
+    var linkColorAccessor = accessorFn(elData.linkColorField);
+    var lineMaterials = {}; // indexed by color
+    elData.links.forEach(function(link) {
+      var color = linkColorAccessor(link);
+      if (!lineMaterials.hasOwnProperty(color)) {
+        lineMaterials[color] = new THREE.LineBasicMaterial({
+          color: colorStr2Hex(color || '#f0f0f0'),
+          transparent: true,
+          opacity: elData.lineOpacity
+        });
+      }
+
+      var geometry = new THREE.BufferGeometry();
+      geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(2 * 3), 3));
+      var lineMaterial = lineMaterials[color];
+      var line = new THREE.Line(geometry, lineMaterial);
+
+      el3d.add(link.__line = line);
+    });
+
+    // Feed data to force-directed layout
+    var isD3Sim = elData.forceEngine !== 'ngraph',
+      layout;
+    if (isD3Sim) {
+      // D3-force
+      (layout = comp.state.d3ForceLayout)
+          .stop()
+          .alpha(1)// re-heat the simulation
+          .numDimensions(elData.numDimensions)
+          .nodes(elData.nodes)
+          .force('link')
+            .id(function (d) {
+              return d[elData.idField]
+            })
+            .links(elData.links);
+    } else {
+      // ngraph
+      var graph = ngraph.graph();
+      elData.nodes.forEach(function (node) {
+        graph.addNode(node[elData.idField]);
+      });
+      elData.links.forEach(function (link) {
+        graph.addLink(link.source, link.target);
+      });
+      layout = ngraph['forcelayout' + (elData.numDimensions === 2 ? '' : '3d')](graph);
+      layout.graph = graph; // Attach graph reference to layout
+    }
+
+    for (var i=0; i<elData.warmupTicks; i++) { layout[isD3Sim?'tick':'step'](); } // Initial ticks before starting to render
+
+    var cntTicks = 0;
+    var startTickTime = new Date();
+    comp.state.onFrame = layoutTick;
+    comp.state.infoEl.setAttribute('value', '');
+
+    //
+
+    function layoutTick() {
+      if (cntTicks++ > elData.cooldownTicks || (new Date()) - startTickTime > elData.cooldownTime) {
+        comp.state.onFrame = null; // Stop ticking graph
+      }
+
+      layout[isD3Sim?'tick':'step'](); // Tick it
+
+      // Update nodes position
+      elData.nodes.forEach(function(node) {
+        var sphere = node.__sphere,
+            pos = isD3Sim
+              ? node
+              : layout.getNodePosition(node[elData.idField]);
+
+        sphere.position.x = pos.x;
+        sphere.position.y = pos.y || 0;
+        sphere.position.z = pos.z || 0;
+      });
+
+      //Update links position
+      elData.links.forEach(function(link) {
+        var line = link.__line,
+            pos = isD3Sim
+                ? link
+                : layout.getLinkPosition(layout.graph.getLink(link.source, link.target).id),
+            start = pos[isD3Sim ? 'source' : 'from'],
+            end = pos[isD3Sim ? 'target' : 'to'],
+            linePos = line.geometry.attributes.position;
+
+        linePos.array[0] = start.x;
+        linePos.array[1] = start.y || 0;
+        linePos.array[2] = start.z || 0;
+        linePos.array[3] = end.x;
+        linePos.array[4] = end.y || 0;
+        linePos.array[5] = end.z || 0;
+
+        linePos.needsUpdate = true;
+        line.geometry.computeBoundingSphere();
+      });
+    }
+
+    //
+
+    function autoColorNodes(nodes, colorByAccessor, colorField) {
+      if (!colorByAccessor || typeof colorField !== 'string') return;
+
+      var colors = d3Chromatic.schemePaired; // Paired color set from color brewer
+
+      var uncoloredNodes = nodes.filter(function(node) { return !node[colorField] });
+      var nodeGroups = {};
+
+      uncoloredNodes.forEach(function(node) { nodeGroups[colorByAccessor(node)] = null });
+      Object.keys(nodeGroups).forEach(function(group, idx) { nodeGroups[group] = idx });
+
+      uncoloredNodes.forEach(function(node) {
+        node[colorField] = colors[nodeGroups[colorByAccessor(node)] % colors.length];
+      });
+    }
+
+    function colorStr2Hex(str) {
+      return isNaN(str) ? parseInt(tinyColor(str).toHex(), 16) : str;
+    }
+  },
+
+
+  tick: function(t, td) {
+    // Update tooltip
+    var centerRaycaster = new THREE.Raycaster();
+    centerRaycaster.setFromCamera(
+        new THREE.Vector2(0, 0), // Canvas center
+        this.state.cameraObj
+    );
+
+    var intersects = centerRaycaster.intersectObjects(this.el.object3D.children)
+        .filter(function(o) { return o.object.name }); // Check only objects with labels
+
+    this.state.tooltipEl.setAttribute('value', intersects.length ? intersects[0].object.name : '' );
+
+    // Run onFrame ticker
+    if (this.state.onFrame) this.state.onFrame();
+  }
+});
